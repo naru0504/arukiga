2cb10ba0af0b9fb0c3badc7613d98d00ac5cb963
Naruhito Kubota <n.kubota@opt.ne.jp>
2019-06-08 16:42:22 +0900

    :sushi: Updated Stories

---
+++ packages/front/src/main/modules/arukigaList/components/ArukigaList.stories.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { ArukigaList } from './ArukigaList'
+
+storiesOf('ArukigaList', module).add('sample', () => <ArukigaList />);
