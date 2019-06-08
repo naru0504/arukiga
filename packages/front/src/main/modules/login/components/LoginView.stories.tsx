e9eb8c2252d766eb703fe38f187057675ae26ab1
Naruhito Kubota <n.kubota@opt.ne.jp>
2019-06-08 16:35:17 +0900

    :book: Stories

---
+++ packages/front/src/main/modules/login/components/LoginView.stories.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { LoginView } from './LoginView'
+
+storiesOf('Login', module).add('sample', () => <LoginView />);
---
+++ packages/front/src/main/modules/logout/components/LogoutView.stories.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { LogoutView } from './LogoutView'
+
+storiesOf('Logout', module).add('sample', () => <LogoutView />);
