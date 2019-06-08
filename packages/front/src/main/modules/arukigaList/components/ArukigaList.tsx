9928b7e9bacb357b3912e5dcfad20051266bf8a7
sisisin <azsisisin@gmail.com>
2019-06-08 16:36:59 +0900

    split file

---
+++ packages/front/src/main/modules/arukigaList/components/ArukigaList.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { Bought } from 'main/services/BoughtRepository';
+
+interface Props {
+  arukiga: Bought[];
+  naikamo: Bought[];
+}
+export const ArukigaList = ({ arukiga, naikamo }: Props) => {
+  return (
+    <div>
+      <h3>ないかも</h3>
+      <ul>
+        {naikamo.map((item, i) => (
+          <li key={`naikamo_${i}`}>{JSON.stringify(item)}</li>
+        ))}
+      </ul>
+      <hr />
+      <h3>ある気が</h3>
+      <ul>
+        {arukiga.map((item, i) => (
+          <li key={`naikamo_${i}`}>{JSON.stringify(item)}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
--- packages/front/src/main/modules/arukigaList/components/ArukigaListView.tsx
+++ packages/front/src/main/modules/arukigaList/components/ArukigaListView.tsx
@@ -3,6 +3,7 @@ import { Button } from 'main/components/Button';
 import { useArukigaListState, useArukigaListActions } from '../module';
 import { useServices } from 'main/services';
 import { useAuthState } from 'main/modules/auth/module';
+import { ArukigaList } from './ArukigaList';
 
 export const ArukigaListView = () => {
   const { boughts } = useArukigaListState();
@@ -15,21 +16,6 @@ export const ArukigaListView = () => {
 
   const arukiga = boughts.filter(b => b.restRate < 1);
   const naikamo = boughts.filter(b => b.restRate >= 1);
-  return (
-    <div>
-      <h3>ないかも</h3>
-      <ul>
-        {naikamo.map((item, i) => (
-          <li key={`naikamo_${i}`}>{JSON.stringify(item)}</li>
-        ))}
-      </ul>
-      <hr />
-      <h3>ある気が</h3>
-      <ul>
-        {arukiga.map((item, i) => (
-          <li key={`naikamo_${i}`}>{JSON.stringify(item)}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  const props = { arukiga, naikamo };
+  return <ArukigaList {...props} />;
 };
